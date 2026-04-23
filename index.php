bb5b0e94bbb2e683b841d54d12bf45bd01b7b9ec
arnoldo <arnoldo>
2026-04-22 21:25:59 -0300

    aula 1

--- index.php
+++ index.php
@@ -1,33 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
 
     <?php
-        echo'<p>Boa noite</p>'
+        $a = 'Boa noite';
+        $b = ' Galera!';
+        echo '<p>' . $a . $b . '</p>';
+
+        $c = 10;
+        $d = 35;
+        $e = $c + $d;
+
+        echo'<p>' . $e . '</p>';
+        echo"<p> A soma de $c + $d é igual a $e </p>";
     ?>
-    <div id="box" style="width:200px;height:100px;padding:16px;border:1px solid #000;">
-        Move mouse over this box
-    </div>
+        <div id="box"
+            style="width:200px;height:100px;padding:16px;border:1px solid #000;">
+            Move mouse over this box
+        </div>
+        <script>
+            const box = document.getElementById("box");
+            box.addEventListener("mouseover", function () {
+                box.innerHTML = "Mouse is over me!";
+            });
+            box.addEventListener("mouseout", function () {
+                box.innerHTML = "Mouse is out!";
+            });
+            box.addEventListener("click", function (){
+                box.innerHTML = "Mouse was clicked";
+            });
+            box.style.backgroundColor = "green";
+            box.style.color = "white";
+            
+        </script>
 
-</body>
-<script>
-
-    const box = document.getElementById("box");
-
-    // Let box listen for mouseover
-    box.addEventListener("mouseover", function () {
-    box.innerHTML = "Mouse is over me!";
-    });
-
-    // Let box listen for mouseout
-    box.addEventListener("mouseout", function () {
-    box.innerHTML = "Move is out!";
-    });
-
-</script>
+    </body>
 </html>
